a5208e9939a05efe72c5e37f6d8f86b2da9fc5d4
KaeTheDev <kaethedev@gmail.com>
2025-12-15 01:03:48 -0500

    Added Admin component. Added ProtectedRoute component around Admin in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,26 @@ import BlogPost from "./pages/BlogPost";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar/Navbar";
+import Admin from "./pages/Admin";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
     <>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Navigate to="/blog" replace />} />
         <Route path="/blog" element={<BlogIndex />} />
         <Route path="/blog/:slug" element={<BlogPost />} />
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
